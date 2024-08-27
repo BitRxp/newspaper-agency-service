fefdc794491f07d9d14e1299fa569523d32a24f1
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-08-27 19:19:54 +0300

    path fixed AUTH_USER_MODEL in settings

--- newspaper_agency_service/settings.py
+++ newspaper_agency_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "newspaper",
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
@@ -123,4 +124,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-AUTH_USER_MODEL = "newspaper_agency_service.Redactor"
+AUTH_USER_MODEL = "newspaper.Redactor"
