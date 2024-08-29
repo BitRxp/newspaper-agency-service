89482c5979996d848cfa0f7c1d5c3cdfeb43b02c
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-08-29 18:08:29 +0300

    Customization of the main page

--- newspaper/views.py
+++ newspaper/views.py
@@ -4,6 +4,19 @@ from newspaper.models import Redactor, Newspaper, Topic
 
 
 def index(request):
-    """View function for the home page of the site."""
 
-    return render(request, "newspaper/index.html")
+    num_redactor = Redactor.objects.count()
+    num_newspaper = Newspaper.objects.count()
+    num_topic = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactor": num_redactor,
+        "num_newspaper": num_newspaper,
+        "num_topic": num_topic,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
