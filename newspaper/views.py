8c2be5b0dff9f9867c938e6a18055de779805f36
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-09-02 17:43:05 +0300

    add login_required and change background photo

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from newspaper.models import Redactor, Newspaper, Topic
 
 
+@login_required
 def index(request):
 
     num_redactor = Redactor.objects.count()
