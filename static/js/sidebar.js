0828ae0ca1d8e0712d81e22df0a7469fb1f2e066
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-09-12 18:38:05 +0300

    replace with "

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sidebar = document.getElementById('sidebar');
-    const sidebarToggle = document.getElementById('sidebarToggle');
+document.addEventListener("DOMContentLoaded", function() {
+    const sidebar = document.getElementById("sidebar");
+    const sidebarToggle = document.getElementById("sidebarToggle");
 
-    if (localStorage.getItem('sidebarActive') === 'true') {
-        sidebar.classList.add('active');
+    if (localStorage.getItem("sidebarActive") === "true") {
+        sidebar.classList.add("active");
     }
 
-    sidebarToggle.addEventListener('click', function() {
-        sidebar.classList.toggle('active');
-        if (sidebar.classList.contains('active')) {
-            localStorage.setItem('sidebarActive', 'true');
+    sidebarToggle.addEventListener("click", function() {
+        sidebar.classList.toggle("active");
+        if (sidebar.classList.contains("active")) {
+            localStorage.setItem("sidebarActive", "true");
         } else {
-            localStorage.setItem('sidebarActive', 'false');
+            localStorage.setItem("sidebarActive", "false");
         }
     });
 });
