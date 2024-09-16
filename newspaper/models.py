2956538f534c88c6fd92f7189b413f6f9ee63d54
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-09-16 17:06:51 +0300

    add to sort topics by name

--- newspaper/models.py
+++ newspaper/models.py
@@ -7,6 +7,9 @@ from django.urls import reverse
 class Topic(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self) -> str:
         return self.name
 
