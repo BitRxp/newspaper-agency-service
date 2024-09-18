9301a898d948be2b8faaa349130318c13ef03555
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-09-18 19:05:21 +0300

    add newspaper-agency-service.onrender.com to ALLOWED_HOSTS

--- newspaper_agency_service/settings.py
+++ newspaper_agency_service/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "newspaper-agency-service.onrender.com"
+]
 
 
 # Application definition
